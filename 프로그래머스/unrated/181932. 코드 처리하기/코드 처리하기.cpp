df29850330a627104950291d93b0c9e8b6436a1e
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-12 15:51:21 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
@@ -7,18 +7,19 @@ string solution(string code) {
     string answer = "";
     bool mode{};
     for(int i=0; code[i]!='\0'; i++){
-        if(code[i]!='1'&&!mode && !(i&1)){
-            answer += code[i];
-        }else if(code[i]!='1'&&mode && (i&1)){
-            answer += code[i];
-        }
-        
         if(code[i]=='1'){
             mode = !mode;
+        }else{
+            switch(mode){
+            case false:
+                if(!(i&1)){answer += code[i];}
+                break;
+            case true:
+                if(i&1){answer += code[i];}
+                break;
+            }
         }
-            
-        
     }
-    if(answer ==""){answer = "EMPTY";}
+    if(answer.empty()){answer = "EMPTY";}
     return answer;
 }
