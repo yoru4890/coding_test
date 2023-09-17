f33ef9c7d8598c66a2cd5aa1ef2acbfcf2de3bd3
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-17 14:59:34 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.09 ms, Memory: 5.54 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.cpp
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.cpp
@@ -7,11 +7,7 @@ using namespace std;
 vector<int> solution(vector<string> intStrs, int k, int s, int l) {
     vector<int> answer;
     for(const auto& e : intStrs){
-        string str;
-        for(int i=s; i<s+l;i++){
-            str += e[i];
-        }
-        int temp {stoi(str)};
+        int temp {stoi(e.substr(s,l))};
         if(k < temp){answer.push_back(temp);}
     }
     return answer;
