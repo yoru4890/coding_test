4cf55f9f1bb420955778ce7f29e325ffde4391cb
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-25 03:57:43 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.20 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -1,38 +1,11 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-bool Check(string& myString, string& pat, int index);
-
 int solution(string myString, string pat) {
-    int answer = 0;
-    for(auto& e : myString){
-        if(e>='A'&&e<='Z'){
-            e -= ('A'-'a');
-        }
-    }
-    for(auto& e : pat){
-        if(e>='A'&&e<='Z'){
-            e -= ('A'-'a');
-        }
-    }
-    
-    int i{};
-    for(const auto& e : myString){
-        if(e== pat[0]){
-            if(Check(myString,pat,i)){
-                return 1;
-            }
-        }
-        i++;
-    }
-    return 0;
-}
-
-bool Check(string& myString, string& pat, int index){
-    for(int i=1, j=index+1; pat[i]!='\0'; i++,j++){
-        if(myString[j] != pat[i]){return false;}
-    }
-    return true;
+    transform(myString.begin(),myString.end(),myString.begin(),::toupper);
+    transform(pat.begin(),pat.end(),pat.begin(),::toupper);
+    return myString.find(pat) != string::npos;
 }
