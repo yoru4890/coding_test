7a2d19480e09bc09493285e1bee975e5037aac5b
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-25 01:14:59 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.cpp
@@ -7,11 +7,7 @@ int solution(vector<int> num_list) {
     int answer = 0;
     for(auto& e : num_list){
         while(e != 1){
-            if(e&1){
-                e = (e-1)/2;
-            }else{
-                e/=2;
-            }
+            e/=2;
             answer++;
         }
     }
