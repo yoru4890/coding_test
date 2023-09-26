e0c3182f1a208cceb275d5d046ccaf9e7f883aca
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-27 01:55:37 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    return vector<int>{numbers.begin()+num1,numbers.begin()+num2+1};
+}
