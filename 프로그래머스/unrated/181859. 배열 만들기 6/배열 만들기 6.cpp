e4bbadef62c7e5870cf49cd11c940e149ab47eb1
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-01 18:30:45 +0900

    [unrated] Title: 배열 만들기 6, Time: 1.06 ms, Memory: 6.48 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for(int i=0; i<arr.size(); i++){
+        if(answer.empty()){
+            answer.emplace_back(arr[i]);
+        }else if(answer.back() == arr[i]){
+            answer.pop_back();
+        }else{
+            answer.emplace_back(arr[i]);
+        }
+    }
+    if(answer.empty()){
+        return {-1};
+    }
+    return answer;
+}
