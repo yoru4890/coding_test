a934ccd2bbb561cbc9dc6bd6acde6fe0f88a206c
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-02 17:44:08 +0900

    [unrated] Title: 배열 비교하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.cpp
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.cpp
@@ -6,19 +6,13 @@ using namespace std;
 
 int solution(vector<int> arr1, vector<int> arr2) {
     int answer = 0;
-    if(arr1.size() > arr2.size()){
-        return 1;
-    }else if(arr1.size() < arr2.size()){
-        return -1;
-    }else{
-        int sum1 = accumulate(arr1.begin(),arr1.end(),0);
-        int sum2 = accumulate(arr2.begin(), arr2.end(),0);
-        if(sum1 > sum2){
-            return 1;
-        }else if(sum1 < sum2){
-            return -1;
-        }else{
-            return 0;
-        }
-    }
+    int size1{static_cast<int>(arr1.size())},
+    size2{static_cast<int>(arr2.size())};
+    if(size1 != size2){
+        return size1 > size2 ? 1 : -1;
+    } 
+    int sum1 {accumulate(arr1.begin(),arr1.end(),0)}, sum2 {accumulate(arr2.begin(), arr2.end(),0)};
+     
+      return sum1 > sum2 ? 1 : sum1 == sum2 ? 0 : -1;
+       
 }
