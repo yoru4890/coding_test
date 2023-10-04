b2db558e0ac017ed0813bedc91120c38e0701e6e
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-04 21:50:04 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer{};
+    answer = date1[0] < date2[0] ? 1 : date1[0] != date2[0] ? 
+                                   0 : date1[1] < date2[1] ? 
+                                   1 : date1[1] != date2[1] ? 
+                                   0 : date1[2] < date2[2] ? 1 : 0;
+    return answer;
+}
