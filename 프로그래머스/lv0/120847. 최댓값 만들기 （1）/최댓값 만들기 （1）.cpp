49914edd51497137ea9944e66c4cf3e8d869a9d8
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-07 15:02:31 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int maxOne{*max_element(numbers.begin(),numbers.end())};
-    numbers.erase(find(numbers.begin(),numbers.end(),maxOne));
-    int maxTwo{*max_element(numbers.begin(),numbers.end())};
-    return maxOne*maxTwo;
+    sort(numbers.begin(),numbers.end(),[](int a,int b){return a > b;});
+    return numbers[0]*numbers[1];
 }
