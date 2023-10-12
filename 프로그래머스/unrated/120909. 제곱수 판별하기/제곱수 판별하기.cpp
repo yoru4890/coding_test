e16a16ab39bfcf86a92f814822ada0e0898acfd9
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-12 21:40:40 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 2;
+    for(int i=1; i<=1000; i++){
+        if(n == i*i){answer = 1; break;}
+    }
+    return answer;
+}
