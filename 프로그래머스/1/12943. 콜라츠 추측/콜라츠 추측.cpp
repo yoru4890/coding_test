e93477216f68dacc522d295c01a72169f2aed6d0
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-10-31 21:42:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long n = num;
+    int answer = 0;
+    while(n > 1){
+        answer++;
+        if(answer>=500){return -1;}
+        if(n&1){
+            n = n*3 + 1;
+        }else{
+            n/=2;
+        }
+    }
+    return answer;
+}
