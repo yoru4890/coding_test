08e2e7de12178926b97fd06ce614825a2b2a62e2
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-12-11 16:14:20 +0900

    [level 1] Title: 소수 찾기, Time: 2.76 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -5,17 +5,21 @@
 using namespace std;
 
 int solution(int n) {
-    vector<bool> isPrime(n+1, true);
+    const int MAX_NUM{1'000'001};
+    
+    vector<bool> isPrime(MAX_NUM, true);
     isPrime[0] = isPrime[1] = false;
     int answer{};
     
-    for(int i{2}; i<=n; i++){
+    for(int i{2}; i<sqrt(MAX_NUM); i++){
         if(isPrime[i]){
-            answer++;
-            for(int j{i*2}; j<=n; j+=i){
+            for(int j{i*2}; j<MAX_NUM; j+=i){
                 isPrime[j] = false;
             }
         }
+    }
+    for(int i{2}; i<=n; i++){
+        if(isPrime[i]){answer++;}
     }
     
     return answer;
