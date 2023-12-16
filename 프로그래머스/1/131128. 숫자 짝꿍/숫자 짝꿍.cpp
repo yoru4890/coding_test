8d037b78ed3174e4c487dbdf934e722465d91083
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-12-17 03:08:39 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -6,7 +6,6 @@ using namespace std;
 string solution(string X, string Y) {
     vector<int> numX(10), numY(10), numCommon(10);
     string answer;
-    bool isHave{}, isZero{true};
     for(const auto& e : X){numX[e-'0']++;}
     for(const auto& e : Y){numY[e-'0']++;}
     
@@ -16,16 +15,11 @@ string solution(string X, string Y) {
     
     for(int i{9}; i>=0;i--){
         if(numCommon[i]){
-            isHave = true;
             while(numCommon[i]--){
                 answer += to_string(i);
             }
         }
     }
     
-    for(const auto& e : answer){
-        if(e!='0'){isZero =false;}
-    }
-    
-    return isHave ? isZero ? "0" : answer : "-1";
+    return answer !="" ? answer[0]=='0'&&answer.size()>1 ? "0" : answer : "-1";
 }
