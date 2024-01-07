bd7ccd1a9ec16bc1cff859c2fca2ead4a5543e7b
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-07 12:58:14 +0900

    [level 2] Title: 올바른 괄호, Time: 0.11 ms, Memory: 3.86 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -8,13 +8,14 @@ bool solution(string s)
     int ccount{};
     
     for(const auto& c : s){
+        if(ccount < 0){return false;}
+        
         if(c == '('){
             ccount++;
         }else{
-            if(ccount <= 0){return false;}
             ccount--;
         }
     }
 
-    return ccount > 0 ? false : true;
+    return ccount == 0;
 }
