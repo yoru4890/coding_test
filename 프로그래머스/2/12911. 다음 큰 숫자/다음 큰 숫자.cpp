f1d04836187c55374158ed585407836ee36a4dee
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-08 14:17:54 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,22 +1,11 @@
 #include <string>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
-int GetBinaryOne(int num){
-    int temp{};
-    while(num){
-        if(num%2){temp++;}
-        num/=2;
-    }
-    return temp;
-}
-
 int solution(int n) {
-    int answer{};
-    int original{GetBinaryOne(n)};
-    for(int i{n+1}; i<=1'000'000; i++){
-        if(original == GetBinaryOne(i)){return i;}
-    }
-    return answer;
+    int num = bitset<20>(n).count();
+    while(bitset<20>(++n).count() != num);
+    return n;
 }
