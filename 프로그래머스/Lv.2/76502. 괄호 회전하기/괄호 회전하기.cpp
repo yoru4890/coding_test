6b529ca163baf088bf0f2d6041eda539ea9ed15c
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-15 01:30:00 +0900

    [level Lv.2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/Lv.2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/Lv.2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,117 +1,79 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <unordered_map>
-
 using namespace std;
 
-int solution(string s) {
+bool IsParenthesis(string& s, int index);
+
+int solution(string s)
+{
     int answer{};
-    vector<int> parenthesisCount(4);
-    unordered_map<char, char> parenthesis;
-    unordered_map<char, int> parenthesisIndex;
-    parenthesis['('] = ')';
-    parenthesis['['] = ']';
-    parenthesis['{'] = '}';
-    parenthesisIndex['('] = parenthesisIndex[')'] = 1;
-    parenthesisIndex['['] = parenthesisIndex[']'] = 2;
-    parenthesisIndex['{'] = parenthesisIndex['}'] = 3;
+    
     for(int i{}; i<s.size(); i++)
     {
-        stack<char> myStack;
-        bool bCant{};
-        for(int j{i}; j<s.size(); j++)
-        {
-            if(myStack.empty())
-            {
-                if(!parenthesis[s[j]])
-                {
-                    bCant = true;
-                    break;
-                }else
-                {
-                    myStack.push(s[j]);
-                    parenthesisCount[parenthesisIndex[s[j]]]++;
-                }
-            }else
-            {
-                if(parenthesis[s[j]])
-                {
-                    myStack.push(s[j]);
-                    parenthesisCount[parenthesisIndex[s[j]]]++;
-                }else
-                {
-                    if(parenthesis[myStack.top()] == s[j])
-                    {
-                        myStack.pop();
-                        parenthesisCount[parenthesisIndex[s[j]]]--;
-                    }else
-                    {
-                        myStack.push(s[j]);
-                        parenthesisCount[parenthesisIndex[s[j]]]--;
-                        if(parenthesisCount[parenthesisIndex[s[j]]] < 0)
-                        {
-                            bCant = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        
-        if(bCant)
-        {
-            continue;
-        }
-        
-        for(int j{}; j<i; j++)
-        {
-            if(myStack.empty())
-            {
-                if(!parenthesis[s[j]])
-                {
-                    bCant = true;
-                    break;
-                }else
-                {
-                    myStack.push(s[j]);
-                    parenthesisCount[parenthesisIndex[s[j]]]++;
-                }
-            }else
-            {
-                if(parenthesis[s[j]])
-                {
-                    myStack.push(s[j]);
-                    parenthesisCount[parenthesisIndex[s[j]]]++;
-                }else
-                {
-                    if(parenthesis[myStack.top()] == s[j])
-                    {
-                        myStack.pop();
-                        parenthesisCount[parenthesisIndex[s[j]]]--;
-                    }else
-                    {
-                        myStack.push(s[j]);
-                        parenthesisCount[parenthesisIndex[s[j]]]--;
-                        if(parenthesisCount[parenthesisIndex[s[j]]] < 0)
-                        {
-                            bCant = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        
-        if(bCant)
-        {
-            continue;
-        }
-        
-        if(myStack.empty())
+        if(IsParenthesis(s,i))
         {
             answer++;
         }
     }
+    
     return answer;
+}
+
+bool IsParenthesis(string& s, int index)
+{
+    stack<char> myStack;
+    for(int i{index}; i<s.size(); i++)
+    {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            myStack.push(s[i]);
+        }else
+        {
+            if(myStack.empty())
+            {
+                return false;
+            }else
+            {
+                if(myStack.top() == '(' && s[i] == ')')
+                {
+                    myStack.pop();
+                }else if(myStack.top() == '{' && s[i] == '}')
+                {
+                    myStack.pop();
+                }else if(myStack.top() == '[' && s[i] == ']')
+                {
+                    myStack.pop();
+                }
+            }
+        }
+    }
+    
+    for(int i{}; i<index; i++)
+    {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            myStack.push(s[i]);
+        }else
+        {
+            if(myStack.empty())
+            {
+                return false;
+            }else
+            {
+                if(myStack.top() == '(' && s[i] == ')')
+                {
+                    myStack.pop();
+                }else if(myStack.top() == '{' && s[i] == '}')
+                {
+                    myStack.pop();
+                }else if(myStack.top() == '[' && s[i] == ']')
+                {
+                    myStack.pop();
+                }
+            }
+        }
+    }
+    
+    return myStack.empty();
 }
