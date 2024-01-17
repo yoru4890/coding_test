3ea4b374409e6201ce9f4399dcddc088885e99ab
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-17 10:07:15 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,16 +5,13 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer{};
-    int size = citations.size();
-    sort(citations.begin(),citations.end());
-    for(int i{}; i<size; i++)
+    sort(citations.begin(),citations.end(),greater<int>());
+    for(int i{}; const auto& e : citations)
     {
-        auto it = upper_bound(citations.begin(),citations.end(),i);
-        if (size - (it-citations.begin()) >= i+1)
+        if(e < ++i)
         {
-            answer = i+1;
+            return i-1;
         }
     }
-    return answer;
+    return citations.size();
 }
