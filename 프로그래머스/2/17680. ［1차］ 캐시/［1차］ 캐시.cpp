8b8629f832d5e5eda49bfcda20fd8c5fca956ef0
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-20 07:03:05 +0900

    [level 2] Title: [1차] 캐시, Time: 0.24 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,18 +1,27 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <unordered_map>
 
 using namespace std;
 
-bool CheckCache(list<string>& cache, const string& s);
-
 int solution(int cacheSize, vector<string> cities) {
-    int answer{};
+    int answer{4};
     list<string> cache;
+    unordered_map<string, list<string>::iterator> cacheMap;
     if(!cacheSize)
     {
         return 5*cities.size();
     }
+    
+    for(auto& c : cities[0])
+    {
+        c = tolower(c);
+    }
+    
+    cache.emplace_back(cities[0]);
+    cacheMap[cities[0]] = cache.begin();
+    
     for(auto& s : cities)
     {
         for(auto& c : s)
@@ -20,40 +29,25 @@ int solution(int cacheSize, vector<string> cities) {
             c = tolower(c);
         }
         
-        if(cache.empty())
+        auto it = cacheMap.find(s);
+        
+        if(it != cacheMap.end())
         {
+            answer++;
+            cache.erase(cacheMap[s]);
             cache.emplace_back(s);
-            answer +=5;
+            cacheMap[s] = --cache.end();
         }else
         {
-            if(CheckCache(cache, s))
+            answer += 5;
+            if(cache.size() >= cacheSize)
             {
-                answer++;
-            }else
-            {
-                answer +=5;
-                if(cache.size() >= cacheSize)
-                {
-                    cache.erase(cache.begin());
-                }
-                cache.emplace_back(s);
+                cacheMap.erase(cache.front());
+                cache.erase(cache.begin());
             }
+            cache.emplace_back(s);
+            cacheMap[s] = --cache.end();
         }
     }
     return answer;
-}
-
-bool CheckCache(list<string>& cache, const string& s)
-{
-    for(auto it{cache.begin()}; it!=cache.end(); it++)
-    {
-        if(*it == s)
-        {
-            cache.erase(it);
-            cache.emplace_back(s);
-            return true;
-        }
-    }
-    
-    return false;
 }
