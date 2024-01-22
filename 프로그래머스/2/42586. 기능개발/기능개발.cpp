8e72ef523167761b6961dbac038260e6b963f7bc
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-22 09:20:59 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,34 +1,36 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
+    vector<int> answer{};
     vector<int> temp;
     for(int index{};const auto& num : progresses)
     {
         temp.emplace_back((100-num + speeds[index] -1)/speeds[index]);
         index++;
     }
-    queue<int> myQueue;
+    stack<int> myStack;
+    int counts{};
     for(const auto& num : temp)
     {
-        if(myQueue.empty() || myQueue.front() >= num)
+        if(myStack.empty())
         {
-            myQueue.push(num);
+            myStack.push(num);
+            counts = 1;
+        }else if(myStack.top() >= num)
+        {
+            counts++;
         }else
         {
-            answer.emplace_back(myQueue.size());
-            while(!myQueue.empty())
-            {
-                myQueue.pop();
-            }
-            myQueue.push(num);
+            answer.emplace_back(counts);
+            myStack.pop();
+            myStack.push(num);
+            counts = 1;
         }
-        
     }
-    answer.emplace_back(myQueue.size());
+    answer.emplace_back(counts);
     return answer;
 }
