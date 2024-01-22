634cfc4ec4051b2af310d94c53b4499cec64fc93
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-22 10:26:59 +0900

    [level 2] Title: 튜플, Time: 0.32 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -6,14 +6,9 @@ using namespace std;
 
 vector<int> solution(string s) {
     const int MAX_NUM{100'000};
-    vector<pair<int,int>> counts(MAX_NUM+1);
+    vector<int> counts(MAX_NUM+1);
     vector<int> answer;
     string temp;
-    
-    for(int i{}; i<=MAX_NUM; i++)
-    {
-        counts[i].second = i;
-    }
     
     for(const auto& c : s)
     {
@@ -22,17 +17,26 @@ vector<int> solution(string s) {
             temp += c;
         }else if(!temp.empty())
         {
-            counts[stoi(temp)].first++;
+            counts[stoi(temp)]++;
             temp.clear();
         }
     }
     
-    sort(counts.begin(), counts.end(),[](const pair<int,int>& a, const pair<int,int>& b)
+    vector<pair<int,int>> result;
+    
+    for(int i{}; i<=MAX_NUM; i++)
+    {
+        if(counts[i])
+        {
+            result.emplace_back(make_pair(counts[i],i));
+        }
+    }
+    
+    sort(result.begin(), result.end(),[](const pair<int,int>& a, const pair<int,int>& b)
          {return a.first > b.first;});
     
-    for(const auto& [num, index] : counts)
+    for(const auto& [num, index] : result)
     {
-        if(num == 0){break;}
         answer.emplace_back(index);
     }
     
