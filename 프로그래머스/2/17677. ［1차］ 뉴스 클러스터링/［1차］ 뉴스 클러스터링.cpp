eebe2c1fb1d16c31256899c76c874fdc95d8e519
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-01-28 14:42:36 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,78 +1,36 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(string str1, string str2) {
     const int MOD{65536};
     int answer{};
-    vector<int> nums1;
-    vector<int> nums2;
-    float commonCount{};
-    
-    for(auto& c : str1)
-    {
-        if((c >= 'a' && c<='z') || (c>='A' && c<='Z'))
-        {
-            c = tolower(c);
-        }
-    }
-    
-    for(auto& c : str2)
-    {
-        if((c >= 'a' && c<='z') || (c>='A' && c<='Z'))
-        {
-            c = tolower(c);
-        }
-    }
+    vector<int> nums1(676);
+    vector<int> nums2(676);
+    float commonCount{}, maxCount;
     
     for(int i{1}; i<str1.size(); i++)
     {
-        if((str1[i] >= 'a' && str1[i]<='z') && (str1[i-1]>='a' && str1[i-1]<='z'))
+        if(isalpha(str1[i]) && isalpha(str1[i-1]))
         {
-            nums1.push_back(str1[i]*26 + str1[i-1]);
+            nums1[((str1[i]&31)-1)*26 + (str1[i-1]&31)-1]++;
         }
     }
     
     for(int i{1}; i<str2.size(); i++)
     {
-        if((str2[i] >= 'a' && str2[i]<='z') && (str2[i-1]>='a' && str2[i-1]<='z'))
+        if(isalpha(str2[i]) && isalpha(str2[i-1]))
         {
-            nums2.push_back(str2[i]*26 + str2[i-1]);
+            nums2[((str2[i]&31)-1)*26 + (str2[i-1]&31)-1]++;
         }
     }
     
-    sort(nums1.begin(), nums1.end());
-    sort(nums2.begin(), nums2.end());
-    
-    int index1{}, index2{};
-    while(index1 < nums1.size() && index2 < nums2.size())
+    for(int i{}; i<676; i++)
     {
-        if(nums1[index1] == nums2[index2])
-        {
-            commonCount++;
-            index1++;
-            index2++;
-        }else if(nums1[index1] > nums2[index2])
-        {
-            index2++;
-        }else
-        {
-            index1++;
-        }
+        commonCount += min(nums1[i],nums2[i]);
+        maxCount += max(nums1[i], nums2[i]);
     }
     
-    float result{};
-    
-    if(nums1.size() || nums2.size())
-    {
-        result = commonCount / (nums1.size() + nums2.size() - commonCount);
-    }else
-    {
-        result = 1;
-    }
-    result *= MOD;
-    
-    return static_cast<int>(result);
+    return maxCount ? static_cast<int>((commonCount/maxCount) * MOD) : 65536;
 }
