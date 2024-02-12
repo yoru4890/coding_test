b4819c452e69071d2dcf8ecc1a91115332df8aee
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-02-12 22:09:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 5.08 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    const int MAX_NUM{10'001};
+    int answer {};
+    int size = topping.size();
+    vector<int> one(MAX_NUM);
+    vector<int> two(MAX_NUM);
+    int countOne{};
+    int countTwo{};
+    for(const auto& e : topping)
+    {
+        if(!one[e])
+        {
+            countOne++;
+        }
+        
+        one[e]++;
+    }
+    
+    for(int i{size-1}; i>0; i--)
+    {
+        one[topping[i]]--;
+        if(!one[topping[i]])
+        {
+            countOne--;
+        }
+        
+        if(!two[topping[i]])
+        {
+            countTwo++;
+        }
+        two[topping[i]]++;
+        
+        if(countOne == countTwo)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
