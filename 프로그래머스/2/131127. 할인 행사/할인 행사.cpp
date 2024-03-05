07dbcb84292f8262c2e45b327f9087b0a0dd7387
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-03-05 20:55:22 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,66 +1,24 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <map>
+#include <iostream>
 
 using namespace std;
 
-int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    int answer{};
-    int flag{};
-    int size = number.size();
-    vector<int> tempNumber(size);
-    unordered_map<string, int> stringIndex;
-    
-    for(int i{1}; const auto& s : want)
+int solution(vector<string> want, vector<int> number, vector<string> discount)
+{
+    int answer=0;
+    map<string,int> m;
+    for(int i=0; i<9; i++) m[discount[i]]++;
+    for(int i=9; i<discount.size(); i++)
     {
-        stringIndex[s] = i++;
+        m[discount[i]]++;
+        bool flag=true;
+        for(int j=0; j<want.size(); j++)
+            if(m[want[j]]!=number[j])
+            { flag=false; break; }
+        if(flag==true) answer++;
+        m[discount[i-9]]--;
     }
-    for(int i{}; i<size; i++)
-    {
-        flag |= 1<<i;
-    }
-    
-    for(int i{}; i<10; i++)
-    {
-        int index {stringIndex[discount[i]]-1};
-        if(index >= 0)
-        {
-            tempNumber[index]++;
-            if(tempNumber[index] >= number[index])
-            {
-                flag &= ~(1<<index);
-            }
-        }
-    }
-    
-    if(!flag){answer++;}
-    
-    for(int i{}; i<discount.size()-10; i++)
-    {
-        int minusIndex {stringIndex[discount[i]]-1};
-        if(minusIndex >= 0)
-        {
-            if(tempNumber[minusIndex])
-            {
-                tempNumber[minusIndex]--;
-                if(tempNumber[minusIndex] < number[minusIndex])
-                {
-                    flag |= 1<<minusIndex;
-                }
-            }
-        }
-        
-        int plusIndex {stringIndex[discount[i+10]]-1};
-        if(plusIndex >= 0)
-        {
-            tempNumber[plusIndex]++;
-            if(tempNumber[plusIndex] >= number[plusIndex])
-            {
-                flag &= ~(1<<plusIndex);
-            }
-        }
-        if(!flag){answer++;}
-    }
-    
     return answer;
 }
